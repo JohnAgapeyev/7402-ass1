42edab3ea1f7d8126b26fcacfac2f2f32c3725d3
John Agapeyev <jagapeyev@gmail.com>
2019-01-17 18:07:53 -0800

    Got basic file reading and plaintext scoring

--- main.c
+++ main.c
@@ -1,6 +1,60 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    puts("Hello World");
+static const char* common_letters = "etaoinshrdlucmfwyp";
+
+unsigned long plaintext_frequency(const unsigned char* input, const size_t len) {
+    unsigned long score = 0;
+    for (size_t i = 0; i < strlen(common_letters); ++i) {
+        for (size_t j = 0; j < len; ++j) {
+            if (tolower(input[j]) == common_letters[i]) {
+                ++score;
+            }
+        }
+    }
+    return score;
+}
+
+unsigned char* caesar_cipher(
+        const unsigned char* input, const size_t len, const unsigned char key) {
+    unsigned char* ciphertext = malloc(len);
+    for (size_t i = 0; i < len; ++i) {
+        if (isalpha(input[i])) {
+            ciphertext[i] = (tolower(input[i]) + key) % 26;
+        }
+    }
+    return ciphertext;
+}
+
+size_t file_len(FILE* fp) {
+    fseek(fp, 0L, SEEK_END);
+    size_t sz = ftell(fp);
+    rewind(fp);
+    return sz;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Needs a filename\n");
+        return 1;
+    }
+    FILE* fp;
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        perror("InFile");
+        return 1;
+    }
+
+    size_t data_len = file_len(fp);
+
+    unsigned char* data = malloc(data_len);
+
+    fread(data, 1, data_len, fp);
+
+    printf("Frequency score: %zu\n", plaintext_frequency(data, data_len));
+
+    fclose(fp);
+
     return 0;
 }
